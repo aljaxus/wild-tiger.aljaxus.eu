66013438fea500120ed0c45d4a2c7269167856b8
aljaxus <aljaxus.dev@gmail.com>
2019-01-02 12:34:18 +0100

    Updated color scheme (theme)

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,4 +5,13 @@ import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify, {
   iconfont: 'mdi',
+  theme: {
+    primary: '#ff5722',
+    secondary: '#ffc107',
+    accent: '#ffeb3b',
+    error: '#f44336',
+    warning: '#ff9800',
+    info: '#03a9f4',
+    success: '#4caf50'
+  }
 })
