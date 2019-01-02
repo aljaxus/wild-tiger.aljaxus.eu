f5988928efee5e222fc4c1dcb4ec7d221b91e81c
aljaxus <aljaxus.dev@gmail.com>
2019-01-02 12:34:43 +0100

    Update store.js

--- src/store.js
+++ src/store.js
@@ -5,12 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    shown: false,
+    mini: false
+  },
+  getters: {
+    drawerState: state => { return state.shown },
+    drawerMini: state => { return state.mini }
   },
   mutations: {
-
+    TOGGLE_DRAWER_STATE (state, newState = null) { state.shown = newState!=null?newState:!state.shown },
+    TOGGLE_DRAWER_MINI (state, newState = null) { state.mini = newState!=null?newState:!state.mini }
   },
   actions: {
-
+    toggleDrawerState (context, newState = null) { context.commit('TOGGLE_DRAWER_STATE', newState) },
+    toggleDrawerMini (context, newState = null) { context.commit('TOGGLE_DRAWER_MINI', newState) }
   }
 })
