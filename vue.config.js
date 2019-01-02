7e59c6414b421d13622b23a9f7cc260d5a5a5ebf
aljaxus <aljaxus.dev@gmail.com>
2019-01-02 12:35:00 +0100

    Added multi-language support

--- vue.config.js
+++ vue.config.js
@@ -21,5 +21,14 @@ module.exports = {
 
   css: {
     sourceMap: true
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
   }
 }
