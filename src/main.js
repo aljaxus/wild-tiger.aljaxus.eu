e97260f91106ed04c729dedec5059800965ca6d1
aljaxus <aljaxus.dev@gmail.com>
2019-01-02 12:36:11 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ new Vue({
   el: '#app',
   router,
   store,
+  i18n,
   render: h => h(App)
 })
