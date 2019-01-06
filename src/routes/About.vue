7fc4ab6ced8c71bc0381b21635939118119398f0
aljaxus <aljaxus.dev@gmail.com>
2019-01-07 00:11:10 +0100

    Update About.vue

--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,21 +1,21 @@
 <template>
-     <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-            <v-flex xs12 sm8 md4>
-                <v-card class="elevation-6">
-                    <v-card-text class="text-xs-center">
-                        <h3>About</h3>
-                    </v-card-text>
-                </v-card>
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+            <v-card class="elevation-6">
+                <v-card-text class="text-xs-center">
+                    <h3>About</h3>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
   export default {
     data: () => ({
-
+      
     })
   }
 </script>
