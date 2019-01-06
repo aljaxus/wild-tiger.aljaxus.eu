7d3a03cb9cb9e65943952ae410f7bf4e4750b30c
aljaxus <aljaxus.dev@gmail.com>
2019-01-06 12:13:03 +0100

    Added main card to every page

--- src/routes/Menu.vue
+++ src/routes/Menu.vue
@@ -1,13 +1,25 @@
 <template>
-  <p>MENU</p>
+     <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md4>
+                <v-card class="elevation-12">
+                    <v-card-text class="text-xs-center">
+                        <h3>Menu</h3>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      //
-    }
+  export default {
+    data: () => ({
+
+    })
   }
-}
 </script>
+
+<style>
+
+</style>
