5b3a62ba5aa198492b6f7c10b854de0e3f9865bd
aljaxus <aljaxus.dev@gmail.com>
2019-01-07 23:29:17 +0100

    Update About.vue

--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,13 +1,13 @@
 <template>
   <v-container fluid fill-height>
     <v-layout align-center justify-center>
-        <v-flex xs12 sm8 md4>
-            <v-card class="elevation-6">
-                <v-card-text class="text-xs-center">
-                    <h3>About</h3>
-                </v-card-text>
-            </v-card>
-        </v-flex>
+      <v-flex xs12 sm12 md12>
+        <v-card class="elevation-6">
+          <v-card-text class="text-xs-center">
+            <h3>About</h3>
+          </v-card-text>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
