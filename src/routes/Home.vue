fe85e4f3bada8bb2e89ccacd2f8dafc719b745f9
aljaxus <aljaxus.dev@gmail.com>
2019-01-07 23:29:23 +0100

    Update Home.vue

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,15 +1,15 @@
 <template>
-     <v-container fluid>
-        <v-layout justify-center>
-            <v-flex xs12 sm12 md12>
-                <v-card class="elevation-6">
-                    <v-card-text class="text-xs-center">
-                        <h3>Home</h3>
-                    </v-card-text>
-                </v-card>
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container fluid>
+    <v-layout justify-center>
+      <v-flex xs12 sm12 md12>
+        <v-card class="elevation-6">
+          <v-card-text class="text-xs-center">
+            <h3>Home</h3>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
