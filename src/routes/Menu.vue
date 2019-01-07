c621298faa8bff3573492cc49a67368b179af231
aljaxus <aljaxus.dev@gmail.com>
2019-01-07 23:29:53 +0100

    Remaking the cards layout (initial idea)

--- src/routes/Menu.vue
+++ src/routes/Menu.vue
@@ -1,35 +1,53 @@
 <template>
-     <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-            <v-flex xs12 sm8 md4>
-                <v-card class="elevation-6">
-                    <v-card-text class="text-xs-center">
-                        <h3>Menu</h3>
-                        <p>
-                          <span
-                            v-for="food in foods"
-                            :key="food.id"
-                          >
-                            <h5>{{food.name}}</h5>
-                            <p>{{food.description}}</p>
-                            <span
-                              v-for="meal in food.meals"
-                              :key="meal.id"
-                            >
-                              Name:{{ meal.name }}<br>
-                              Description:{{ meal.description }}<br>
-                              Price:{{ meal.price }}<br>
-                              Additions:{{ meal.additions }}<br>
-                              <br>
-                            </span>
-                            <br><br><br><br>
-                          </span>
-                        </p>
-                    </v-card-text>
+  <v-container fluid>
+            
+            <v-layout
+              v-for="food in foods"
+              :key="food.id"
+              row
+              wrap
+            >
+              <v-flex
+                xs12
+              >
+                <h5>{{food.name}}</h5>
+                <p>{{food.description}}</p>
+              </v-flex>
+
+              <v-flex 
+                xs6 
+                sm4 
+                md4 
+                lg3
+                class="pa-2"
+                v-for="(meal, id) in food.items"
+                :key="id"
+              >
+                <v-card
+                  height="100%"
+                  hover
+                >
+                  <v-img
+                    :src="meal.imgUrl != null && meal.imgUrl != '' ? meal.imgUrl : './img/transparent-512px.png'"
+                    aspect-ratio="2.75"
+                  ></v-img>
+
+                  <v-card-title>
+                    <h3 style="font-weight:bold; font-size: 20px;">{{meal.name}}</h3>
+                  </v-card-title>
+                  <v-card-text>
+                    <div>{{meal.description}}</div>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-btn flat color="orange">Share</v-btn>
+                    <v-btn flat color="orange">Explore</v-btn>
+                  </v-card-actions>
                 </v-card>
-            </v-flex>
-        </v-layout>
-    </v-container>
+              </v-flex>
+
+            </v-layout>
+  </v-container>
 </template>
 
 <script>
