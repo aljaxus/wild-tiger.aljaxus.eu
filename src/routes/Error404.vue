3857c06add4457a3c95a6b93f1dc521b8fd9554f
aljaxus <aljaxus.dev@gmail.com>
2019-01-07 23:29:20 +0100

    Update Error404.vue

--- src/routes/Error404.vue
+++ src/routes/Error404.vue
@@ -1,17 +1,17 @@
 <template>
-     <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-            <v-flex xs12 sm8 md4>
-                <v-card class="elevation-6">
-                    <v-card-text class="text-xs-center">
-                        <h3>{{ $t("error404_title") }}</h3>
-                        <p>{{ $t("error404_message") }}</p>
-                        <v-btn :to="{name:'home'}" color="error"><v-icon>mdi-arrow-left</v-icon><span>{{ $t("error404_btnTxt") }}</span></v-btn>
-                    </v-card-text>
-                </v-card>
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-6">
+          <v-card-text class="text-xs-center">
+            <h3>{{ $t("error404_title") }}</h3>
+            <p>{{ $t("error404_message") }}</p>
+            <v-btn :to="{name:'home'}" color="error"><v-icon>mdi-arrow-left</v-icon><span>{{ $t("error404_btnTxt") }}</span></v-btn>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
